b6e10c1a751d91987506762a1a70e92c4db761f8
Christopher Brown <cb_yeti@Julias-MacBook-Air.local>
2017-03-08 16:41:55 -0500

    process.env port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+port = process.env.PORT || 8080;
+
 app.get('*', function(req, res){
     var ipaddress = req.headers["x-forwarded-for"];
     var language = req.headers['accept-language'].slice(0, req.headers['accept-language'].indexOf(";"));
@@ -9,6 +11,6 @@ app.get('*', function(req, res){
     res.send(JSON.stringify({'ipaddress': ipaddress, "language": language, 'software': software}));
 });
 
-app.listen(8080, function(){
-    console.log("app listening on port 8080");
+app.listen(port, function(){
+    console.log("app listening on port " + port);
 })
